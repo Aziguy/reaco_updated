13cb07ca83bd70ca7b482e4b2baf8bf40f23777d
Aziguy <monsieur_hernandez@hotmail.com>
2023-05-10 10:25:02 +0200

    Set db config

--- recipes/settings.py
+++ recipes/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {'default': dj_database_url.config(default='postgres://postgres:postgres@localhost/recipes_db')}
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:postgres@localhost/recipes_db')}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
