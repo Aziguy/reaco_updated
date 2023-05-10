a087a5b974abcacd7c06ec93ff8a939feef9d753
Aziguy <monsieur_hernandez@hotmail.com>
2023-05-10 10:14:58 +0200

    Comment django_heroku from settings.py

--- recipes/settings.py
+++ recipes/settings.py
@@ -14,7 +14,7 @@ import os.path
 from pathlib import Path
 import dj_database_url
 from decouple import config
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
